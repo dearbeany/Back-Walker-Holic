43b29cbb10f0d6e130c36445c59218f19d79caab
mayril <inho806901@gmail.com>
2022-11-21 16:56:08 +0900

    c 20221121

--- src/main/java/com/ssafy/running/model/dto/User.java
+++ src/main/java/com/ssafy/running/model/dto/User.java
@@ -5,54 +5,64 @@ public class User {
 	private String username;
 	private String password;
 	private String campus;
-
-	public User() {
-	}
-
-	public User(String userId, String username, String password, String campus) {
+	private String phone;
+	private String avatar;
+	private String email;
+	
+	public User(String userId, String username, String password, String campus, String phone, String avatar,
+			String email) {
 		super();
 		this.userId = userId;
 		this.username = username;
 		this.password = password;
 		this.campus = campus;
+		this.phone = phone;
+		this.avatar = avatar;
+		this.email = email;
 	}
-
 	public String getUserId() {
 		return userId;
 	}
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getCampus() {
 		return campus;
 	}
-
 	public void setCampus(String campus) {
 		this.campus = campus;
 	}
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", campus=" + campus
-				+ "]";
+	public String getPhone() {
+		return phone;
 	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 
 }
